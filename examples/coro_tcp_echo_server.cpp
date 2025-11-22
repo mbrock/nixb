47b296090fc78b04f711c389adc8c03934e9470f
Mikael Brockman <mikael@brockman.se>
2025-11-22 05:50:20 +0200

    Squashed 'subprojects/libcoro/' content from commit 879e5a9
    git-subtree-dir: subprojects/libcoro
    git-subtree-split: 879e5a9d9fde05a9469dfdbd642e06607fc838d9

---
+++ examples/coro_tcp_echo_server.cpp
@@ -0,0 +1,79 @@
+#include <coro/coro.hpp>
+
+auto main() -> int
+{
+    auto make_tcp_echo_server = [](std::unique_ptr<coro::io_scheduler>& scheduler) -> coro::task<void>
+    {
+        auto make_on_connection_task = [](coro::net::tcp::client client) -> coro::task<void>
+        {
+            std::string buf(1024, '\0');
+
+            while (true)
+            {
+                // Wait for data to be available to read.
+                co_await client.poll(coro::poll_op::read);
+                auto [rstatus, rspan] = client.recv(buf);
+                switch (rstatus)
+                {
+                    case coro::net::recv_status::ok:
+                        // Make sure the client socket can be written to.
+                        co_await client.poll(coro::poll_op::write);
+                        client.send(std::span<const char>{rspan});
+                        break;
+                    case coro::net::recv_status::would_block:
+                        break;
+                    case coro::net::recv_status::closed:
+                    default:
+                        co_return;
+                }
+            }
+        };
+
+        co_await scheduler->schedule();
+        coro::net::tcp::server server{scheduler, coro::net::tcp::server::options{.port = 8888}};
+
+        while (true)
+        {
+            // Wait for a new connection.
+            auto pstatus = co_await server.poll();
+            switch (pstatus)
+            {
+                case coro::poll_status::event:
+                {
+                    auto client = server.accept();
+                    if (client.socket().is_valid())
+                    {
+                        scheduler->spawn(make_on_connection_task(std::move(client)));
+                    } // else report error or something if the socket was invalid or could not be accepted.
+                }
+                break;
+                case coro::poll_status::error:
+                case coro::poll_status::closed:
+                case coro::poll_status::timeout:
+                default:
+                    co_return;
+            }
+        }
+
+        co_return;
+    };
+
+    std::vector<std::unique_ptr<coro::io_scheduler>> schedulers;
+    std::vector<coro::task<void>> workers{};
+
+    const std::size_t count = std::thread::hardware_concurrency();
+
+    schedulers.reserve(count);
+    workers.reserve(count);
+
+    for (size_t i = 0; i < std::thread::hardware_concurrency(); ++i)
+    {
+        auto& scheduler = schedulers.emplace_back(coro::io_scheduler::make_unique(
+            coro::io_scheduler::options{
+                .execution_strategy = coro::io_scheduler::execution_strategy_t::process_tasks_inline}));
+
+        workers.emplace_back(make_tcp_echo_server(scheduler));
+    }
+
+    coro::sync_wait(coro::when_all(std::move(workers)));
+}
